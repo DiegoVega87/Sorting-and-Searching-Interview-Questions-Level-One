1ded74ed7aa2c632fb417431f96ef32028a025dd
Diego Vega <diegovega@me.com>
2023-07-23 11:06:03 +0200

    Add examples

--- src/ArrayMerging.java
+++ src/ArrayMerging.java
@@ -5,6 +5,17 @@ public class ArrayMerging {
     * We need to efficiently merge the two databases into a single sorted array to process the data.
     * Can you write a function that takes in the two arrays and returns a merged array sorted in non-decreasing order?
     *
+    * Example 1:
+    *
+    *   arr1 = {1, 3, 5, 7};
+    *   arr2 = {2, 4, 6, 8};
+    *   mergedArr should be {1, 2, 3, 4, 5, 6, 7, 8}
+    *
+    *
+    * Example 2:
+    *   arr1 = {1, 2, 3, 4};
+    *   arr2 = {5, 6, 7, 8};
+    *   mergedArr should be {1, 2, 3, 4, 5, 6, 7, 8}
     *
     * */
 }
