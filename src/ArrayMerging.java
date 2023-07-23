2a7e7b03b324dd23817be9131a8355d1b61254d1
Diego Vega <diegovega@me.com>
2023-07-23 10:49:52 +0200

    Add problem explanation

---
+++ src/ArrayMerging.java
@@ -0,0 +1,10 @@
+public class ArrayMerging {
+
+    /*
+    * Our startup has two databases containing important information, both sorted in non-decreasing order.
+    * We need to efficiently merge the two databases into a single sorted array to process the data.
+    * Can you write a function that takes in the two arrays and returns a merged array sorted in non-decreasing order?
+    *
+    *
+    * */
+}
