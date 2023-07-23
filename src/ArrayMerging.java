677120bd24f4ce8b988dc22db820fb30577586a1
Diego Vega <diegovega@me.com>
2023-07-23 11:18:41 +0200

    Add test cases

--- src/ArrayMerging.java
+++ src/ArrayMerging.java
@@ -21,6 +21,29 @@ public class ArrayMerging {
     *
     * */
 
+    public static void main(String[] args){
+
+        int[] arr1 = {1,3,5,7};
+        int[] arr2 = {2,4,6,8};
+
+        System.out.println(Arrays.toString(mergeSortedArrays(arr1,arr2)));
+        arr1 = new int[]{1, 2, 3, 4};
+        arr2 = new int[]{5, 6, 7, 8};
+
+        System.out.println(Arrays.toString(mergeSortedArrays(arr1,arr2)));
+
+        arr1 = new int[0];
+        arr2 = new int[]{5, 6, 7, 8};
+
+        System.out.println(Arrays.toString(mergeSortedArrays(arr1,arr2)));
+
+        arr1 = new int[0];
+        arr2 = new int[0];
+
+        System.out.println(Arrays.toString(mergeSortedArrays(arr1,arr2)));
+
+
+    }
 
     public static int[] mergeSortedArrays(int[] arr1, int[] arr2){
 
