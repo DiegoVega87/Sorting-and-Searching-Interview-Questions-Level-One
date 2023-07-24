4723e53ae329cc4890a95d206ca1a0037bf4d53b
Diego Vega <diegovega@me.com>
2023-07-24 10:11:36 +0200

    Add examples

--- src/FindingShipments.java
+++ src/FindingShipments.java
@@ -5,5 +5,15 @@ public class FindingShipments {
     * that can efficiently search for the target value in the 2D matrix.
     * The algorithm should return True if the identifier is found, else False.
     *
+    * Example 1:
+    *   matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    *   target = 5;
+    *   Output: true
+    *
+    * Example 2:
+    *   int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    *   target = 10;
+    *   Output: False
+    *
      * */
 }
