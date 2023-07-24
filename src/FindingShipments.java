6825dc9ccdd2b8b6a9a5ad5d696726cdeb69713f
Diego Vega <diegovega@me.com>
2023-07-24 11:42:22 +0200

    Add solution

--- src/FindingShipments.java
+++ src/FindingShipments.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+
+
 public class FindingShipments {
     /*
     * As a startup in the logistics industry, we have a massive matrix containing information about different shipments.
@@ -16,4 +19,30 @@ public class FindingShipments {
     *   Output: False
     *
      * */
+
+
+    public static boolean searchShipment(int[][] matrix, int target){
+
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+
+        int start = 0;
+        int end = (rows * columns)-1;
+
+        while(start <= end){
+
+            int mid = start + (end - start)/2;
+            int midValue = matrix[mid / columns][mid % columns];
+
+            if(midValue == target){
+                return true;
+            }else if(target > midValue){
+                start = mid + 1;
+            }else{
+                end = mid -1;
+            }
+        }
+
+        return false;
+    }
 }
