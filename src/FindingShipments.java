418d9610f410d78a572a14d76115e47168d990ef
Diego Vega <diegovega@me.com>
2023-07-24 10:07:42 +0200

    Add problem explanation

---
+++ src/FindingShipments.java
@@ -0,0 +1,9 @@
+public class FindingShipments {
+    /*
+    * As a startup in the logistics industry, we have a massive matrix containing information about different shipments.
+    * We need to quickly search for a specific shipment based on its unique identifier and are looking for an algorithm
+    * that can efficiently search for the target value in the 2D matrix.
+    * The algorithm should return True if the identifier is found, else False.
+    *
+     * */
+}
